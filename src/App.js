3d21521a27c0baae9316ba622be70176cb8392e5
Andre Pires <brainshutdown@gmail.com>
2022-01-10 23:35:50 +0000

    Dark-mode added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from 'react';
+import data from './data';
+import Article from './Article';
 
-function App() {
-  return <h2>dark mode starter</h2>
+function getStorageTheme() {
+	return localStorage.getItem('theme') || 'light-theme';
 }
 
-export default App
+function App() {
+	const [theme, setTheme] = useState(getStorageTheme());
+
+	useEffect(() => {
+		document.documentElement.className = theme;
+		localStorage.setItem('theme', theme);
+	}, [theme]);
+
+	return (
+		<main className={theme}>
+			<nav>
+				<div className='nav-center'>
+					<h1>Over Reacted</h1>
+					<button
+						className='btn'
+						onClick={() => {
+							if (theme === 'light-theme') {
+								setTheme('dark-theme');
+							} else {
+								setTheme('light-theme');
+							}
+						}}>
+						toggle
+					</button>
+				</div>
+			</nav>
+			<section className='articles'>
+				{data.map((article) => {
+					return <Article key={article.id} {...article} />;
+				})}
+			</section>
+		</main>
+	);
+}
+
+export default App;
--- src/Article.js
+++ src/Article.js
@@ -1,7 +1,23 @@
-import React from 'react'
-import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
-}
+import React from 'react';
+// import moment from 'moment';
+const Article = ({ title, length, date, snippet }) => {
+	return (
+		<article className='post'>
+			<h2>{title}</h2>
+			<div className='post-info'>
+				<span>
+					{date.toLocaleDateString('en-gb', {
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric',
+						timeZone: 'utc',
+					})}
+				</span>
+				<span>{length} min read</span>
+			</div>
+			<p>{snippet}</p>
+		</article>
+	);
+};
 
-export default Article
+export default Article;
